e99490073b05ded46d9c3cf790ccbed5fc3f9fb4
Aditya Vinayak Singh <129313396+adidotio@users.noreply.github.com>
2026-07-30 18:10:59 +0530

    Update hreflang links and script in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,12 +10,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link href="https://nextjs-test.research-flow.in/" hrefLang="x-default" rel="alternate" />
-        <link href="https://ja.nextjs-test.research-flow.in/" hrefLang="ja" rel="alternate" />
-        <link href="https://hi.nextjs-test.research-flow.in/" hrefLang="hi" rel="alternate" />
-        <link href="https://gu.nextjs-test.research-flow.in/" hrefLang="gu" rel="alternate" />
-        <script src="https://script-cdn.multilipi.com/static/JS/page_translations.js" multilipi-key="b3ccbf20-020f-4326-8896-72735e726ad8" mode="auto" data-pos-x="50" data-pos-y="50" crossOrigin="anonymous" defer></script>
-      </head>
+        <link href="https://nextjs-test.research-flow.in/" hreflang="x-default" rel="alternate">
+        <link href="https://gu.nextjs-test.research-flow.in/" hreflang="gu" rel="alternate">
+        <link href="https://cs.nextjs-test.research-flow.in/" hreflang="cs" rel="alternate">
+        <link href="https://kn.nextjs-test.research-flow.in/" hreflang="kn" rel="alternate">
+        <script src="https://script-cdn.multilipi.com/static/JS/page_translations.js" multilipi-key="337f8db8-19f4-44e3-a6a3-ab3c76dcd060" mode="auto" data-pos-x="50" data-pos-y="50" crossorigin="anonymous" defer></script>
+    </head>
       <body>
         <nav className="navbar">
           <Link href="/" className="nav-logo gradient-text">Research Flow</Link>
